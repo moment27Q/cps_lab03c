e26229720e972ddfd4b6faf39f1cc290b4056aed
Joaquin11-08-2006 <jorge.casas@tecsup.edu.pe>
2025-09-01 17:31:57 -0500

    controlador commit

--- src/main/java/pe/edu/tecsup/LAB03/StudentController.java
+++ src/main/java/pe/edu/tecsup/LAB03/StudentController.java
@@ -1,4 +1,9 @@
 package pe.edu.tecsup.LAB03;
 
 public class StudentController {
+
+    // 👤 Usuario 1: Inicializar datos
+    public String initStudents() {
+        return "Estudiantes inicializados ✅";
+    }
 }
